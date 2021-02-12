e53afbf3eba5ba6a60e4432c684c5c582462c399
Dancan Chibole <dancankingstar@gmail.com>
2021-02-11 19:26:19 -0800

    pagination

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,8 @@ STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'authentication.User'
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
